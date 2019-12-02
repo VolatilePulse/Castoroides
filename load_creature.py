28993f09f0dfc5e7e4cae127d7ab42b46b947529
coldino <28345893+coldino@users.noreply.github.com>
2019-12-02 01:30:03 +0000

    Switch export loader's stats to intervals

--- load_creature.py
+++ load_creature.py
@@ -1,9 +1,10 @@
 from configparser import ConfigParser
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from enum import IntEnum
 from typing import List, Optional
 
 from lib.intervals import Float, Interval
+from utils import possible_inputs_from_rounded
 
 
 class Status(IntEnum):
@@ -19,9 +20,9 @@ class CreatureInput:
     status: Status
     level: int
     imprint: Interval
-    stats: List[Float]
-    wild_levels: Optional[List[int]]
-    dom_levels: Optional[List[int]]
+    stats: List[Interval]
+    wild_levels: Optional[List[int]] = None
+    dom_levels: Optional[List[int]] = None
 
 
 def load_export_init(filename: str):
@@ -34,13 +35,18 @@ def load_export_init(filename: str):
     creature.name = parser['Dino Data']['DinoNameTag']
     creature.species_bp = parser['Dino Data']['DinoClass']
     creature.status = Status.Tamed
+
     if parser['Dino Data'].get('ImprinterName', '').strip():
         creature.status = Status.Bred
+
     if parser['Dino Data']['BabyAge'] != '1.000000':
         raise ValueError("Not fully grown")
+
     creature.level = int(parser['Dino Data']['CharacterLevel'])
     creature.imprint = Interval(parser['Dino Data']['DinoImprintingQuality'])
 
-    creature.stats = [Float(txt) for txt in parser['Max Character Status Values'].values()]
+    creature.stats = [
+        possible_inputs_from_rounded(Float(txt), digits=6) for txt in parser['Max Character Status Values'].values()
+    ]
 
     return creature
